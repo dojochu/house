666f75547818691da5d48f6caaa18817362add74
Kao <stephanie.kao@accenturefederal.com>
2016-09-19 12:27:36 -0400

    remove customizable files

--- src/main_script.py
+++
@@ -1,8 +0,0 @@
-import src.data_prep as dp
-import src.data_analysis as da
-import importlib
-from matplotlib import pyplot
-
-data = dp.read_house_data('C:\\Users\\stephanie.kao\\PycharmProjects\\house_price_prediction\\resources\\')
-print(data.head(10))
-print(data.axes)
